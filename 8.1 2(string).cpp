f69fdf7076085c56f129064c729ffc04f20660b6
Vasyl <vasulkydruch@gmail.com>
2022-03-30 10:51:31 +0300

    Добавьте файлы проекта.

---
+++ 8.1 2(string).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void Change(string& s);
+
+int main()
+{
+    string str; // оголошується масив символів
+    cout << "Enter string:" << endl;
+    getline(cin, str); // в масив вводяться введені символи
+    Change(str); // викликається функція заміни одинакових символів 
+    cout << "Result: " << str << endl;
+}
+
+void Change(string& str)
+{
+    size_t found = str.find("!!!");
+    while (found != string::npos)
+    {
+        str.replace(found, 3, "**"); // заміна однакових символів на зірочки
+        found = str.find("!!!");
+    }
+}
